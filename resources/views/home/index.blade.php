f1bd644c1f5868827b1aa09e0809cbbd276aecee
SaraBarrett <sara.c.mon@gmail.com>
2024-03-21 15:36:45 +0000

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h4>Olá, estou em casa!</h4>
+    <ul>
+        <a href="{{ route('home.welcome') }}">
+            <li>Welcome</li>
+        </a>
+        <a href="{{ route('home.hello') }}">
+            <li>Hello</li>
+        </a>
+        <a href="{{ route('users.all') }}">
+            <li>
+                Users</li>
+        </a>
+        <a href="{{ route('tasks.all') }}">
+            <li>
+                tarefas</li>
+        </a>
+    </ul>
+</body>
+
+</html>
