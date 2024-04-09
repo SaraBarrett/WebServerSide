43f4aeb8b47f37caf62b071a44888a36024587be
SaraBarrett <sara.c.mon@gmail.com>
2024-04-09 09:54:17 +0100

    controlelr

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,17 +8,25 @@ class IndexController extends Controller
 {
     public function homePage(){
         $sum = $this->sum(1,3);
+        $helloVar = $this->helloFunc();
 
         return view('home.index');
     }
 
     public function helloWorld(){
         $sum = $this->sum(1,3);
+        $helloVar = $this->helloFunc();
+
         return view('hello');
     }
 
 
     protected function sum($num1, $num2){
         return $num1+$num2;
+    }
+
+    protected function helloFunc(){
+        $hello = 'Olá mundo, estamos a aprender web';
+        return $hello;
     }
 }
